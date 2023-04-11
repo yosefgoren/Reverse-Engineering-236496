e18c6156e06f0d015fa6cf5af2581f2e731619ff
Tomer Bitan <tbitan@gmail.com>
2023-04-11 19:15:56 +0300

    wrote challage C solution

--- challenge.c
+++ challenge.c
@@ -1,36 +1,45 @@
-#include <string>
-#include <iostream>
-using namespace std;
-int main() {
-    string  line;
+#include <stdio.h>
+int compare(const char* a, const char* b){
+    int i=0;
+    while(a[i] && b[i]){
+        if(a[i] != b[i]){
+            return 0;
+        }
+        i++;
+    }
+    return !a[i] && !b[i];
+    return 0;
+}
+
+int main(){
     int wood = 0;
     int brick = 0;
     int wool = 0;
     int grain = 0;
     int ore = 0;
+    char line[200];
 
-    while (getline(cin, line)){
-        if (line == "road"){
+    while (fgets(line, sizeof(line)-1, stdin)){
+        if (compare(line, "road\n")){
             wood++;
             brick++;
         }
-        if (line == "settlement"){
+        else if (compare(line,"settlement\n")){
             wood++;
             brick++;
             wool++;
             grain++;
         }
-        if (line == "city"){
+        else if (compare(line,"city\n")){
             grain += 2;
             ore += 3;
         }
-        if (line == "development"){
+        else if (compare(line,"development\n")){
             grain++;
             ore++;
             wool++;
         }
-
     }
-    cout << "You total is - wood:" << wood <<", brick:" << brick<<", wool:"<<wool<<", grain:"<<grain<<", ore:"<<ore<< endl;
+    printf("%02d %02d %02d %02d %02d\n", wood, brick, wool, grain, ore);
     return 0;
 }
---
+++ makefile
@@ -0,0 +1,5 @@
+run: challange.exe
+	type in.txt | challange.exe
+
+challange.exe:
+	gcc challange.c -o challange
